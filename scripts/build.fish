9d12533b98af9aa631cf0563dc42670e8d51a9b9
Max Neunhoeffer <max@arangodb.com>
2018-01-13 00:57:50 +0100

    Change script.

--- scripts/build.fish
+++ scripts/build.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/fish
+if test $PARALLELISM = ""
+    set -x PARALLELISM 64
+end
 cd /ArangoDB
 mkdir -p build
 cd build
-cmake $CMAKE -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/gcc -DUSE_JEMALLOC=Off -DSTATIC_EXECUTABLES=On ..
-nice make $argv
+cmake $argv -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/gcc -DUSE_JEMALLOC=Off -DSTATIC_EXECUTABLES=On ..
+nice make -j$PARALLELISM
