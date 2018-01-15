4b18dadb50cadad2a434e307e257c9facebdd7b1
Max Neunhoeffer <max@arangodb.com>
2018-01-15 23:05:07 +0100

    Minor changes to keep alive and ccache config.

--- scripts/build.fish
+++ scripts/build.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/fish
+date > /tmp/usedstamp
 if test $PARALLELISM = ""
     set -x PARALLELISM 64
 end
 cd /ArangoDB
 mkdir -p build
+mkdir -p ccache
 cd build
+set -x CCACHE_DIR /ArangoDB/ccache
 cmake $argv -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/gcc -DUSE_JEMALLOC=Off -DSTATIC_EXECUTABLES=On ..
 nice make -j$PARALLELISM
