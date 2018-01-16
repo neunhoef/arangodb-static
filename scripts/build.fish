3b435f653a584e8bd77eadf40dfec8e2b3c96822
Max Neunhoeffer <max@arangodb.com>
2018-01-16 08:57:43 +0100

    Sort out ccache.

--- scripts/build.fish
+++ scripts/build.fish
@@ -4,9 +4,10 @@ if test $PARALLELISM = ""
     set -x PARALLELISM 64
 end
 cd /ArangoDB
+rm -rf build
 mkdir -p build
-mkdir -p ccache
+mkdir -p .ccache
 cd build
-set -x CCACHE_DIR /ArangoDB/ccache
+set -x CCACHE_DIR /ArangoDB/.ccache
 cmake $argv -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/gcc -DUSE_JEMALLOC=Off -DSTATIC_EXECUTABLES=On ..
 nice make -j$PARALLELISM
